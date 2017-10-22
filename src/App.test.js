0e0141c15888104f3752f44961d8253035e28ca4
Carvallegro <carvallegro@users.noreply.github.com>
2017-10-22 13:30:15 +1100

    merge bugfix (#7)
    * Add CodeClimate badges
    
    * Add TravisCI badge
    
    * Updated travis CI
    
    * Adding deployment feature
    
    * add target branch in ci script
    
    * Hotfix/gh pages deployment (#4)
    
    * Changed settings to see build folder in github pages
    
    * changes folder to work around gitignore
    
    * Bugfix/gh pages rendering (#6)
    
    * changed target branch for bugfix purpose
    
    * Updated nvm version + rolledback travis
    
    * Ejected create-react-app config
    
    * Changed config to set homepage and code cleaning
    
    * fixed yaml format
    
    * rolledback

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,9 @@
 import React from 'react'
-import Enzyme, { shallow } from 'enzyme';
+import Enzyme, { shallow } from 'enzyme'
 import App from './App'
-import Adapter from 'enzyme-adapter-react-16';
+import Adapter from 'enzyme-adapter-react-16'
 
-Enzyme.configure({ adapter: new Adapter() });
+Enzyme.configure({ adapter: new Adapter() })
 it('renders', () => {
   const result = shallow(<App />)
   expect(result).toMatchSnapshot()
