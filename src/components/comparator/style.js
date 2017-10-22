77ece11a3f8068c8a9a38858943c13db906f4d5f
Carvallegro <valentin.carmignac@gmail.com>
2017-10-22 15:01:39 +1100

    Enhanced default text content and sall comparator changes

--- src/components/comparator/style.js
+++ src/components/comparator/style.js
@@ -1,6 +1,8 @@
 import styled from 'styled-components'
 
 export const Wrapper = styled.div`
+  font-family: ${({typeface}) => typeface};
   background-color: grey;
   flex-grow: 1;
+  overflow-y: scroll;
 `
